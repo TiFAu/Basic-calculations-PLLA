c4564a83424d3d92f3202ac7e56caf8b283a3f55
TiFAu <turchin.anton2@gmail.com>
2023-12-03 21:01:03 +0200

    changed function resultToExcel.onclick for use under Windows

--- js/index.js
+++ js/index.js
@@ -2393,15 +2393,19 @@ calc.onclick = function () {
   console.log (initialDate)
   console.log (intermediateValues)
   console.log (calculationResults)
-  resultToExcel.onclick = (() => { //функция вывода результатов расчета в файл excel
+  resultToExcel.onclick = (() => {
     const uri = 'data:application/vnd.ms-excel;base64,',
-    template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
-    base64 = (s) => window.btoa(unescape(encodeURIComponent(s))),
-    format = (s, c) => s.replace(/{(\w+)}/g, (m, p) => c[p])
+      template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>',
+      btoaUtf16 = (s) => {
+        return btoa(unescape(encodeURIComponent(s)));
+      },
+      format = (s, c) => s.replace(/{(\w+)}/g, (m, p) => c[p]);
+  
     return (name) => {
-      const table = document.getElementById ('tableResults')
-      const ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-      window.location.href = uri + base64(format(template, ctx))
-    }
+      const table = document.getElementById('tableResults');
+      const ctx = { worksheet: name || 'Worksheet', table: table.innerHTML };
+      window.location.href = uri + btoaUtf16(format(template, ctx));
+    };
   })();
+  
 }
